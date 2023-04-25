9cdf648de31d9871589ae3788ff483b6dbae3aeb
Pravin Kumar R <66785258+pravin1101@users.noreply.github.com>
2023-04-25 20:19:42 +0530

    Delete MADLIBS directory

--- MADLIBS/Madlibs.py
+++
@@ -1,8 +0,0 @@
-adj = input("Adjective:")
-verb = input("verb:")
-verb2 = input("verb2:")
-famous_person = input("famous person:")
-madlib = f"computer programming is so {adj}. It makes me so exicited all the time because it is so {adj}.\
-and i love learning {verb}, so stay hydrated {verb2} and you're a {famous_person}."
-print(madlib)
-
